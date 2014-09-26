1dadb1fc570d80f08701708b76ee477ce4b90d2a
19jerry <19jerry@gmail.com>
2014-09-26 15:27:09 +0800

    a

--- src/PushLog.py
+++ src/PushLog.py
@@ -7,12 +7,14 @@ import traceback
 
 VERSION = '0.1.0'
 
-DSTURL = "http://dq.baidu.com/nocipher?pid=rtlog&ver=1.0&cdn=yunyi&service=pcs&type=access&log=%s&node=%s"
+DSTURL = "http://dq.baidu.com/nocipher?pid=rtlog&ver=1.0&cdn=ctdns&service=pcs&type=access&log=%s&node=%s"
 LOG_BASE = '/logs/'
 PID = '/var/run/PushLog.pid'
 THREADNUM = 30
 DEBUG = 0
 DAY_MIN = ''
+SERVERCONF = ''
+SERVERS = {}
 
 TM = ''
 socket.setdefaulttimeout(10) 
@@ -24,26 +26,70 @@ def PrintLog(s, tt=''):
         sys.stdout.flush()
 
 
+def GetServerList():
+	global SERVERCONF
+	global SERVERS
+	last_mtime = 0
+	while 1:
+		server_tmpd = {}
+		mtime = os.stat(SERVERCONF).st_mtime
+		if mtime > last_mtime:
+			fp = open(SERVERCONF, 'rb')
+			cont = fp.read().split('\n')
+			fp.close()
+			for k in cont:
+				if len(k)  == 0:continue
+				if k.find('#') >= 0 :continue
+				try:
+					items = k.split()
+					sip = items[0]
+					idc = items[1]
+					server_tmpd[sip] = idc
+				except:	
+					pass
+			SERVERS = server_tmpd
+			PrintLog('Server Num %s' % len(SERVERS.keys()), 'info')
+			last_mtime = mtime
+
+		time.sleep(60)
+
+
 class Handler(object):
 	def __init__(self, wid):
 		pass
 
 	def process(self, line):
-		l = urllib.quote(line)
-                p_url = DSTURL % (l, 'bj-ct')
+		"""112.251.255.160 - p0.qq.sogoucdn.com [26/Sep/2014:13:19:05 +0800] 0.175 "GET /u/dhqq/v2/js/jquery.min.js HTTP/1.1" 502 718 "http://xiaoshuo.sogou.com/" "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36 SE 2.X MetaSr 1.0" "112.251.255.160" 123.151.116.75 "-" 1411708745.206 "-" MISS
+		"""
+		idc = ''
+		st = 0
+		global SERVERS
 		try:
-			self.push(s, self.uri)
+			items = line.split(' ')
+			s_ip = items[-5]
+			idc = SERVERS.get(s_ip, '') 
 		except:
-			pass
+			traceback.print_exc()
+		l = urllib.quote(line)
+                p_url = DSTURL % (l, idc)
+		try:
+			st = self.push(p_url)
+		except:
+			traceback.print_exc()
+		return st
 
 	def push(self, surl):
+		st = 0
                 try: 
-			h =  urllib2.urlopen(sul)
+			h =  urllib2.urlopen(surl)
 			h.read(10)
 			status = h.code
+			if status == 200:
+				st = 1		
                 except Exception , e:
                         PrintLog(e)
                         status = "Error"
+		return st
         
 
 class TaskManager(object):
@@ -57,6 +103,7 @@ class TaskManager(object):
                 self.n = 0
                 self.exit = 0
                 self.finished = 0
+		self.error = 0
         
         # add task to task list 
         def addtask(self, line):
@@ -82,11 +129,13 @@ class TaskManager(object):
                 while not self.exit:
                         self.lock.acquire()
                         finished = self.finished
+			error = self.error
                         self.finished = 0
+			self.error = 0
                         unfinished = len(self.tasklist)
                         self.lock.release()
 
-                        s = '*'*20 + "finished %s unfinished %s" % (finished, unfinished) + '*'*20
+                        s = '*'*20 + "finished %s error %s unfinished %s" % (finished, error, unfinished) + '*'*20
                         PrintLog(s)
                         time.sleep(5)
 
@@ -114,11 +163,14 @@ class TaskManager(object):
                         	# Create Handler
                         	hd = Handler(worker_id)
                         	# Hander Process Task
-                        	hd.process(task)
+                        	isok = hd.process(task)
 			except:
 				PrintLog(traceback.print_exc())		
                         self.lock.acquire()
-                        self.finished += 1
+			if isok:
+                        	self.finished += 1
+			else:
+				self.error += 1
                         self.lock.release()
 
 def gen_next_min(day_min):
@@ -166,8 +218,9 @@ def main():
 	try:
         	log_base = sys.argv[1]
 		thread_num = int(sys.argv[2])
+		server_file = sys.argv[3]
 		try:
-			debug = sys.argv[3]
+			debug = sys.argv[4]
 			if debug == "debug":
 				debug = 1
 		except:
@@ -175,11 +228,13 @@ def main():
 		global DEBUG
 		global LOG_BASE
 		global THREADNUM
+		global SERVERCONF
 		THREADNUM = thread_num
 		LOG_BASE = log_base
+		SERVERCONF = server_file
 		DEBUG = debug
 	except:
-		print "Usage: PushLog log_dir thread_num [debug](example PushLog /logs/ 30 )"
+		print "Usage: PushLog log_dir thread_num server_file [debug](example PushLog /logs/baidulog/ /logs/servers.txt 30)"
 		sys.exit()
 		pass
         global PID
@@ -196,6 +251,8 @@ def main():
         fp.write(str(pid))
         fp.close()
 
+	# start GetServerList Thread
+	thread.start_new_thread(GetServerList, ())
 
         global TM
 
